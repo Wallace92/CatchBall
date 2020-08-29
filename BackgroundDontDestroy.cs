d43e62d174b264119e5f54fb695184824671885a
Wallace92 <mateuszwallace@gmail.com>
2020-08-29 17:03:49 +0200

    Background and Coins

---
+++ BackgroundDontDestroy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BackgroundDontDestroy : MonoBehaviour
+{
+   
+    //Two prefabs: skyOne and SkyTwo, one is next to the other
+    public GameObject backgroundOne;
+    public GameObject backgroundTwo;
+
+
+    void Update()
+    {
+        // Move both prefabs with the same speed in -x direction, if the speed will increases an additional component of s = v * t should be included
+        float speed = 1;
+        backgroundOne.transform.localPosition += new Vector3(-8f, 0, 0) * Time.deltaTime * speed;
+        backgroundTwo.transform.localPosition += new Vector3(-8f, 0, 0) * Time.deltaTime * speed;
+        
+        //Shift both backgrounds when the -x component is less them size of image. Some offset of 18 is included to satisfy the currently speed = 10
+        if (backgroundOne.transform.localPosition.x < -6000f)
+        {
+            backgroundOne.transform.localPosition = new Vector3(5982f, 0f, 0f);
+        }
+        if (backgroundTwo.transform.localPosition.x < -6000f)
+        {
+            backgroundTwo.transform.localPosition = new Vector3(5982f, 0f, 0f);
+        }
+
+        // Acces to width of both images
+        // float width = backgroundOne.GetComponent<SpriteRenderer>().bounds.size.x;
+        // float width2 = backgroundTwo.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+}
---
+++ CoinsTextScript.cs
@@ -0,0 +1,81 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsTextScript : MonoBehaviour
+{
+
+    public Text cointText;
+    public static bool coinUpdate = false;
+    float overallScore;
+
+    void Start()
+    {
+        overallScore = 0;
+
+        if (Swap.scoresEasy.Count == 0)
+        {
+            
+            foreach (var item in Swap.scoresEasy)
+            {
+                overallScore += item;
+            }
+        }
+        else
+        {
+            //If files was opened just add the appropriate scores do not open it again! It will multiply the scores
+            foreach (var item in Swap.scoresEasy)
+            {
+                overallScore += item;
+            }
+        }
+
+        if (Swap.scoresMedium.Count == 0)
+        {
+           
+            foreach (var item in Swap.scoresMedium)
+            {
+                overallScore += item;
+            }
+        }
+        else
+        {
+            //If files was opened just add the appropriate scores do not open it again! It will multiply the scores
+            foreach (var item in Swap.scoresMedium)
+            {
+                overallScore += item;
+            }
+        }
+        if (Swap.scoresHard.Count == 0)
+        {
+            
+            foreach (var item in Swap.scoresHard)
+            {
+                overallScore += item;
+            }
+        }
+        else
+        {
+            //If files was opened just add the appropriate scores do not open it again! It will multiply the scores
+            foreach (var item in Swap.scoresHard)
+            {
+                overallScore += item;
+            }
+        }
+
+        //Initial value of coins
+        cointText.text = "SCORE: " + overallScore.ToString();
+    }
+
+    void Update()
+    {
+        // In GameControl script the coinUpdate is changed after pass the level so the coin amout is updated as well
+        if (coinUpdate == true)
+        {
+            cointText.text = "SCORE: " + GameControl.instance.scoresToLeaderboard.ToString();
+            coinUpdate = false;
+        }
+    }
+
+ 
+}
